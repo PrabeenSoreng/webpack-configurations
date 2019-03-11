427b6b7eeec6aef8f2c8bd38fab4316746f13451
Prabeen <prabeensoreng2119@gmail.com>
2019-03-11 11:04:20 +0530

    CSS configuration

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,8 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }]
   }
 };
