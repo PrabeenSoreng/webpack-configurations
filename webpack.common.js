c4f7a586d6a99067811cf4c4a07ca18c5b3d42ac
Prabeen <prabeensoreng2119@gmail.com>
2019-03-13 11:20:17 +0530

    html-loader, file-loader and clean-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,18 @@ module.exports = {
   ],
   module: {
     rules: [
-      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] }
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+      { test: /\.html$/, use: ["html-loader"] },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
+      }
     ]
   }
 };
